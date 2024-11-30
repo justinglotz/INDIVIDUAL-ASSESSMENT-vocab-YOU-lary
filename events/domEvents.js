4a1fb8b3d4afcd37e4351cee4ee4d7d95ba047a6
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2024-11-30 11:14:47 -0500

    Feat: add ability for user to delete entries

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,5 +1,6 @@
 import addEntryForm from '../components/forms/addEntryForm';
-import { getSingleEntry } from '../api/entryData';
+import { deleteEntry, getEntries, getSingleEntry } from '../api/entryData';
+import showEntries from '../pages/entries';
 
 const domEvents = (user) => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
@@ -7,6 +8,18 @@ const domEvents = (user) => {
     if (e.target.id.includes('edit-entry-btn')) {
       const [, firebaseKey] = e.target.id.split('--');
       getSingleEntry(firebaseKey).then((entryObj) => addEntryForm(entryObj, user));
+    }
+    // Click event for deleting an entry
+    if (e.target.id.includes('delete-entry')) {
+      // eslint-disable-next-line no-alert
+      if (window.confirm('Want to delete?')) {
+        console.warn('CLICKED DELETE ENTRY', e.target.id);
+        const [, firebaseKey] = e.target.id.split('--');
+
+        deleteEntry(firebaseKey).then(() => {
+          getEntries(user.uid).then(showEntries);
+        });
+      }
     }
   });
 };
